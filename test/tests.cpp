6e114ba893341ceeb6a993ddd53b0b481d95dee7
WeAreNotEternal <99015955+WeAreNotEternal@users.noreply.github.com>
2022-04-04 10:18:32 +0700

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -26,3 +26,22 @@ CTEST(arithmetic_S, test1) {
   int res = check(x1, x2, y1, y2, r1, r2);
   ASSERT_EQUAL(expected, res);
 }
+CTEST(arithmetic_S, test3) {
+  int x1 = 10;
+  int x2 = 1;
+  int y1 = 10;
+  int y2 = 1;
+  int r1 = 1;
+  int r2 = 2;
+  const int expected = 1;
+  int res = check(x1, x2, y1, y2, r1, r2);
+  ASSERT_EQUAL(expected, res);
+}
+CTEST(Test_s, test4) {
+  int x1 = 10;
+  int y1 = 5;
+  int r1 = 5;
+  const int expected = 300;
+  int res = circle(x1, y1, r1);
+  ASSERT_EQUAL(expected, res);
+}
